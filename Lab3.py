876841a3ef19a934d26cf0348c7e517784b797d7
hannahmatrangola <hjmatrangola@loyola.edu>
2021-10-01 13:06:38 -0400

    FINISHED

--- Lab3.py
+++ Lab3.py
@@ -5,29 +5,34 @@
 # Program Inputs: Weight of package and distance the package is sent.
 # Program Outputs: Cost of shipping
 
-wieght = float(input("Enter a weight: "))
-distance_miles = float(input("Enter distance: "))
+wieght = float(input("Enter a weight in kg: "))
+distance_miles = float(input("Enter distance in miles: "))
 
-if wieght <= 2:
-    price = ((distance_miles / 500) * 1.10)
-    print("Shipping price: ", price)
+
+if distance_miles < 10 or distance_miles > 3000:
+    print("Error, distance is out of range!")
 
 else:
-    if wieght > 2 and wieght <= 6:
-        price = ((distance_miles / 500) * 2.20)
-        print("Shipping price: ", price)
-
-    else:
-        if wieght > 6 and wieght <= 10:
-            price = ((distance_miles / 500) * 3.70)
+        if wieght <= 2:
+            price = ((distance_miles / 500) * 1.10)
             print("Shipping price: ", price)
 
         else:
-            if wieght > 10 and wieght <= 20:
-                price = ((distance_miles / 500) * 4.80)
+            if wieght > 2 and wieght <= 6:
+                price = ((distance_miles / 500) * 2.20)
                 print("Shipping price: ", price)
 
             else:
-                if wieght > 20:
-                    print("Error, Out of range!")
+                if wieght > 6 and wieght <= 10:
+                    price = ((distance_miles / 500) * 3.70)
+                    print("Shipping price: ", price)
+
+                else:
+                    if wieght > 10 and wieght <= 20:
+                        price = ((distance_miles / 500) * 4.80)
+                        print("Shipping price: ", price)
+
+                    else:
+                        if wieght > 20:
+                            print("Error, wieght is out of range!")
 
